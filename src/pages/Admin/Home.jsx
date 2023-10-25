d1d830a5df5536becea51d8c52d7f13d36912992
alejandrosb8 <alejandrosanchezb08@gmail.com>
2023-10-25 09:00:46 -0400

    feat: charts

--- src/pages/Admin/Home.jsx
+++ src/pages/Admin/Home.jsx
@@ -1,5 +1,6 @@
 import Layout from '../../layouts/Default';
-import { Title, Container, Text, Stack, Box } from '@mantine/core';
+import { Title, Container, Text, Stack, Box, SimpleGrid } from '@mantine/core';
+import Chart from 'react-apexcharts';
 
 function AdminHome() {
   return (
@@ -10,10 +11,71 @@ function AdminHome() {
             <Title order={1}>Bienvenido</Title>
             <Text>Esta es la página de inicio del administrador</Text>
           </Box>
-          <Text>
-            Usa el menú de la izquierda para navegar (si estás en un dispositivo móvil, presiona el botón de la esquina
-            superior izquierda para abrir el menú)
-          </Text>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              columnGap: '4rem',
+              marginTop: '2rem',
+              flexWrap: 'wrap',
+              rowGap: '2rem',
+            }}
+          >
+            <Chart
+              options={{
+                chart: {
+                  id: 'basic-bar',
+                },
+                xaxis: {
+                  categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+                },
+              }}
+              series={[
+                {
+                  name: 'series-1',
+                  data: [30, 40, 45, 50, 49, 60],
+                },
+              ]}
+              type="bar"
+              width="450"
+            />
+            <Chart
+              type="donut"
+              width="380"
+              series={[44, 55, 13, 43, 22]}
+              options={{
+                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+              }}
+            />
+            <Chart
+              type="donut"
+              width="380"
+              series={[44, 55, 13, 43, 22]}
+              options={{
+                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+              }}
+            />
+            <Chart
+              options={{
+                chart: {
+                  id: 'basic-bar',
+                },
+                xaxis: {
+                  categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+                },
+              }}
+              series={[
+                {
+                  name: 'series-1',
+                  data: [30, 40, 45, 50, 49, 60],
+                },
+              ]}
+              type="bar"
+              width="450"
+            />
+          </div>
         </Stack>
       </Container>
     </Layout>
